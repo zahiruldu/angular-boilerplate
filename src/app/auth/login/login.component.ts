94fb279877fa05585fc99a0bfddd7d2532198065
zahiruldu <zahirul.arb@gmail.com>
2017-10-20 00:18:10 +0600

    auth service

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
       if (this.authService.isLoggedIn) {
         // Get the redirect URL from our auth service
         // If no redirect has been set, use the default
-        let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/admin';
+        let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/home';
 
         // Set our navigation extras object
         // that passes on our global query params and fragment
